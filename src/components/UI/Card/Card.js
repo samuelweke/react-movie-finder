9639f568d78326e0d0073b11de7127a8139a66f4
wekesamuel <wekesamuel@yahoo.com>
2020-04-26 19:10:51 +0100

    format code with prettier

--- src/components/UI/Card/Card.js
+++ src/components/UI/Card/Card.js
@@ -1,17 +1,13 @@
 import React from "react";
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
 import classes from "./Card.module.css";
 
-const Card = ({src, alt, title, date}) => {
+const Card = ({ src, alt, title, date }) => {
     return (
         <div className={`card border-0 ${classes.Card}`}>
             <div className={classes.CardImgWrapper}>
-                <img
-                    src={src}
-                    className="h-100 w-100"
-                    alt={alt}
-                />
+                <img src={src} className="h-100 w-100" alt={alt} />
             </div>
             <div className={classes.CardBody}>
                 <h2>{title}</h2>
@@ -26,7 +22,6 @@ Card.propTypes = {
     alt: PropTypes.string,
     title: PropTypes.string,
     date: PropTypes.string,
-}
+};
 
 export default Card;
-
