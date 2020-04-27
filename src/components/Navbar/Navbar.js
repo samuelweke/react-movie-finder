182d8b3ec4f0c44f28472231bdc08c2a06670e2f
wekesamuel <wekesamuel@yahoo.com>
2020-04-27 20:50:58 +0100

    Add container class to navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,17 @@
 import React from "react";
-import NavItems from './NavItems/NavItems'
+import NavItems from "./NavItems/NavItems";
+
+import classes from "./Navbar.module.css";
 
 const Navbar = () => {
     return (
-        <header className="navbar navbar-expand navbar-light bg-light">
-            <a className="navbar-brand mr-auto" href="/#">MovieFinder</a>
-            <NavItems />
+        <header className={`navbar navbar-expand ${classes.Navbar}`}>
+            <div className="container">
+                <a className="navbar-brand mr-auto" href="/#">
+                    MovieFinder
+                </a>
+                <NavItems />
+            </div>
         </header>
     );
 };
