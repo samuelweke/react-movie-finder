478a4a67dbbaae6fad724474ab7f40d04a7fe50a
wekesamuel <wekesamuel@yahoo.com>
2020-04-27 14:45:40 +0100

    feat: Implement Trending Section

---
+++ src/components/Trending/Trending.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+import axios from "../../lib/axiosInstance";
+import Card from "../UI/Card/Card";
+import CardSkeleton from "../UI/Skeleton/CardSkeleton";
+
+import classes from "./Trending.module.css";
+
+class Trending extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: false,
+            showError: false,
+            errorMsg: "",
+            trending: {},
+        };
+    }
+
+    componentDidMount = () => {
+        this.getTrendingData();
+    };
+
+    getTrendingData = async () => {
+        try {
+            this.setState({ isLoading: true });
+            const res = await axios.get("/trending/movie/week");
+
+            if (res.status === 200) {
+                this.setState({
+                    isLoading: false,
+                    trending: res.data,
+                });
+            }
+        } catch (error) {}
+    };
+
+    render = () => {
+        const {
+            isLoading,
+            trending: { results },
+        } = this.state;
+
+        let loader = [1, 2, 3, 4, 5, 6, 7].map((a, i) => (
+            <CardSkeleton key={i} />
+        ));
+
+        let movieCard =
+            results &&
+            results.length > 0 &&
+            results.map((movie) => (
+                <Card
+                    key={movie.id}
+                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                    alt={movie.title}
+                    title={movie.original_title}
+                    date={movie.release_date}
+                />
+            ));
+
+        return (
+            <main>
+                <section className="container">
+                    <h1>Trending</h1>
+                    <div className="d-flex card-scroll">
+                        {isLoading ? loader : movieCard}
+                    </div>
+                </section>
+            </main>
+        );
+    };
+}
+
+export default Trending;
