0093b3c9a949d3809480f12cddb022e29e9a7761
wekesamuel <wekesamuel@yahoo.com>
2020-04-29 21:04:56 +0100

    Add Popular route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "../src/containers/Home/Home";
+import Home from "./pages/Home/Home";
+import Popular from "./pages/Movies/Popular/Popular";
 
 function App() {
     return (
         <Router>
             <Switch>
                 <Route exact path="/" component={Home} />
+                <Route exact path="/movie" component={Popular} />
             </Switch>
         </Router>
     );
